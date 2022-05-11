b29d5f99700f169db4afd1a08a2f5461cb116ab9
Maria Eduarda Ferreira de Oliveira Brito Seichas <85970732+MariaEduardaSeichas@users.noreply.github.com>
2022-05-11 21:03:33 +0000

    rotas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var {usuario} = require("./models");
+var {usuario, empresa} = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -8,30 +8,57 @@ app.use(express.urlencoded({
 }))
 
 
-app.get("/",  async function(req,res){
+app.get("/usuarios",  async function(req,res){
 var resultado = await usuario.findAll();
 res.json(resultado);
 })
 
-app.post("/", async function(req,res){
+app.post("/usuarios", async function(req,res){
   var resultado = await usuario.create(req.body);
   res.json(resultado);
 })
 
-app.put("/:id", async function(req, res) {
+app.put("/usuarios/:id", async function(req, res) {
   var resultado = await usuario.findByPk(req.params.id);
   resultado.nome = req.body.nome
   var salvar = await resultado.save()
   res.json(salvar)
 })
 
-app.delete("/:id", async function(req, res) {
+app.delete("/usuarios/:id", async function(req, res) {
   var resultado = await usuario.destroy({ where: { id: req.params.id }});
   res.json(resultado)
 })
 
-app.get("/:id", async function(req, res) {
+app.get("/usuarios/:id", async function(req, res) {
   var resultado = await usuario.findByPk(req.params.id);
+  res.send(resultado)
+})
+
+app.get("/empresas",  async function(req,res){
+var resultado = await empresa.findAll();
+res.json(resultado);
+})
+
+app.post("/empresas", async function(req,res){
+  var resultado = await empresa.create(req.body);
+  res.json(resultado);
+})
+
+app.put("/empresas/:id", async function(req, res) {
+  var resultado = await empresa.findByPk(req.params.id);
+  resultado.nome = req.body.nome
+  var salvar = await resultado.save()
+  res.json(salvar)
+})
+
+app.delete("/empresas/:id", async function(req, res) {
+  var resultado = await empresa.destroy({ where: { id: req.params.id }});
+  res.json(resultado)
+})
+
+app.get("/empresas/:id", async function(req, res) {
+  var resultado = await empresa.findByPk(req.params.id);
   res.send(resultado)
 })
 
